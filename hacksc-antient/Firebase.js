9c2d6618aac675b0fc91ce7fdf291ac6780efc08
Float Teephop Keerasuntonpong <floatkeera@gmail.com>
2019-04-13 03:53:21 -0700

    firebase integration preliminary

---
+++ hacksc-antient/Firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyA9-yk6n965ESvvgiN3YDIBjO9zqP7qo1o",
+  authDomain: "hacksc-44d94.firebaseapp.com",
+  databaseURL: "https://hacksc-44d94.firebaseio.com",
+  projectId: "hacksc-44d94",
+  storageBucket: "",
+  messagingSenderId: "589213859485"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
---
+++ hacksc-antient/PictureMarkerView.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+export default class PictureMarkerView extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={};
+    }
+
+}
